27628854be2f2a4f007e9ad1b8aca0faabb25cc2
Mazin Al-Ani <mazinal-ani@Mazins-MacBook-Pro.local>
2023-01-06 22:58:54 -0500

    Fixed reset button in contact form

--- src/components/contactform.jsx
+++ src/components/contactform.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+/*import { React } from 'react';
+import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
 import './stylesheet.css';
-import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const DataForm = () => (
   <>
     <h1 className='form_descriptions'>Enter the Following Fields:</h1>
     <Formik
+
+      const formik = useFormik({
+
+      }
+      );
       initialValues={{ name: "", email: "", message: "", acceptedTerms: false }}
       validate={(values) => {
         const errors = {};
@@ -43,19 +48,34 @@ const DataForm = () => (
             <label className='inner_field_padding'>
               Name: 
             </label>
-            <Field type="text" name="name" />
+            <Field 
+            type="text" 
+            name="name" 
+            onChange={formik.handleChange}
+            value={formik.values.message}
+            />
             <ErrorMessage name="name" component="span" />
           </div>
           <div className='field_padding'>
             <label className='inner_field_padding' htmlFor="email">Email:</label>
-            <Field type="email" name="email" />
+            <Field 
+            type="email" 
+            name="email" 
+            onChange={formik.handleChange}
+            value={formik.values.email}/>
             <ErrorMessage name="email" component="span" />
           </div>
           <div className='field_padding'>
             <label className='inner_field_padding'>
               Message:
             </label>
-            <Field type="text" name="message" style={{ height: 150 }}/>
+            <Field 
+            type="text" 
+            name="message" 
+            style={{ height: 150 }}
+            onChange={formik.handleChange}
+            value={formik.values.message}
+            />
             <ErrorMessage name="message" component="span" />
           </div>
           <div className='field_padding'>
@@ -65,7 +85,7 @@ const DataForm = () => (
           </div>
           <button
             type="button"
-            onClick={handleReset}
+            onClick={formik.handleReset}
             disabled={!dirty || isSubmitting}
           >
             Reset
@@ -79,4 +99,116 @@ const DataForm = () => (
   </>
 );
 
-export default DataForm;
+export default DataForm;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { React, useState } from 'react';
+import Typed from "react-typed";
+import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
+import './stylesheet.css';
+
+
+const DataForm = () => {
+
+  const [responseData, setResponseData] = useState(null);
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      fetch('http://127.0.0.1:5000/encrypt', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              values
+            }),
+          })
+          .then(response => response.json())
+          .then(data => setResponseData(JSON.stringify(data)))
+          }
+    }
+  );
+
+  return(
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+      <div className='form_field'>
+        <label htmlFor='name'>
+          Name: 
+        </label>
+        <textarea
+        id="name"
+        rows="1"
+        name="name"
+        type="name"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+        />
+      </div>
+      <div className='form_field'>
+        <label htmlFor='name'>
+          Email: 
+        </label>
+        <textarea
+        id="email"
+        rows="1"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        />
+      </div>
+      <div className='form_field'>
+        <label htmlFor='message'>
+          Message: 
+        </label>
+        <textarea 
+        id="message"
+        rows="10"
+        name="message"
+        type="message"
+        onChange={formik.handleChange}
+        value={formik.values.message}
+        />
+      </div>
+      <button type="submit">Submit</button>
+      <button onClick={formik.handleReset}>Reset</button>
+      </form>
+      <div className='field_padding'>
+      {responseData === null ? (
+        <Typed
+        strings={["Awaiting input..."]}
+        typeSpeed={25}
+        backSpeed={10}
+        loop/>
+      ) : (
+        <div>
+          <h1 className='field_padding'>Here is your encrypted message:</h1>
+          <h1 className='form_descriptions'>{responseData.replace(/"/g, '')}</h1>
+        </div>
+      )}
+      </div>
+    </div>
+  );
+};
+
+export default DataForm
